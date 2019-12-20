d0aaf7c439e38ee41c25be715e4b0cef8bfd886e
Simona Pentangelo <56032063+SimonaPentangelo@users.noreply.github.com>
2019-12-21 00:59:25 +0100

    Create Invito.java

---
+++ src/easyjob/entity/Invito.java
@@ -0,0 +1,54 @@
+package easyjob.entity;
+
+public class Invito {
+
+	//Parametri
+	private String titolo;
+	private String corpo;
+	private Azienda azienda;
+	private Inoccupato inoccupato;
+	
+	//Costruttore
+	public Invito(String titolo, String corpo, Azienda azienda, Inoccupato inoccupato) {
+		super();
+		this.titolo = titolo;
+		this.corpo = corpo;
+		this.azienda = azienda;
+		this.inoccupato = inoccupato;
+	}
+
+	//Getters and setters
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public String getCorpo() {
+		return corpo;
+	}
+
+	public void setCorpo(String corpo) {
+		this.corpo = corpo;
+	}
+
+	public Azienda getAzienda() {
+		return azienda;
+	}
+
+	public void setAzienda(Azienda azienda) {
+		this.azienda = azienda;
+	}
+
+	public Inoccupato getInoccupato() {
+		return inoccupato;
+	}
+
+	public void setInoccupato(Inoccupato inoccupato) {
+		this.inoccupato = inoccupato;
+	}
+
+	
+}
