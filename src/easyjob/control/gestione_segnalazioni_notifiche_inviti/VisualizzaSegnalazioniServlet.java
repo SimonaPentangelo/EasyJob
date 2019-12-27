6b9d8075221965645def39fc8e711d8f7fc2e171
Arkemus2302 <51193421+Arkemus2302@users.noreply.github.com>
2019-12-27 17:37:09 +0100

    Update VisualizzaSegnalazServlet e jsp

--- src/easyjob/control/gestione_segnalazioni_notifiche_inviti/VisualizzaSegnalazioniServlet.java
+++ src/easyjob/control/gestione_segnalazioni_notifiche_inviti/VisualizzaSegnalazioniServlet.java
@@ -6,6 +6,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+import easyjob.entity.Annuncio;
+import easyjob.entity.Segnalazione;
+import easyjob.model.ManagerAnnunci;
+import easyjob.model.ManagerSegnalazioni;
 
 /**
  * Servlet implementation class VisualizzaSegnalazioniServlet
@@ -25,8 +35,23 @@ public class VisualizzaSegnalazioniServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		ManagerSegnalazioni manager = new ManagerSegnalazioni();
+		String redirect = "";
+		List<Segnalazione> segnalazioni = new ArrayList<>();
+		
+		try{
+			segnalazioni = manager.visualizzaElencoSegnalazioni();
+			request.getSession().setAttribute("segnalazioni", segnalazioni);
+			redirect = "/WEB-PAGES/view/visualizzaSegnalazioni.jsp";
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		response.sendRedirect(request.getContextPath()+ redirect);
+		
+		
+		
+		
 	}
 
 	/**
