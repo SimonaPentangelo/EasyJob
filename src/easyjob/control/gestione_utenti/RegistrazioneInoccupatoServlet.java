69729168b2c4a9cb9818accc126606462c8b0b6f
Simona Pentangelo <56032063+SimonaPentangelo@users.noreply.github.com>
2020-01-15 13:50:17 +0100

    Revisione ODD (forse è quasi finita)

--- src/easyjob/control/gestione_utenti/RegistrazioneInoccupatoServlet.java
+++ src/easyjob/control/gestione_utenti/RegistrazioneInoccupatoServlet.java
@@ -19,10 +19,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-import org.apache.catalina.servlet4preview.RequestDispatcher;
-
-import com.google.gson.Gson;
-
 import easyjob.entity.Inoccupato;
 import easyjob.model.ManagerUtenti;
 
